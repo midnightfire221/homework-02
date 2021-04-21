bb65da0f1d22dffe659d3092fe3beb58e8e3c826
Thida <sslorvan@uncg.edu>
2021-04-20 22:24:48 -0400

    No Stealing API Keys

---
+++ HW2/src/config.java
@@ -0,0 +1,5 @@
+public class config {
+    protected static final String API_KEY1 = "a4b3e6ca892711ae53dca75ea5de2912";
+    protected static final String API_KEY2 = "239c5cde3emsha1c1ca5f5df29b5p11ff94jsn19abbfc94e90";
+
+}
